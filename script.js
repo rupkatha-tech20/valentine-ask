887c4a22db6df91dc448bb4c2ce8c3d9995b8360
rupkatha-tech20 <chakravortyrupkatha@gmail.com>
2026-02-11 13:13:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "Yeaaii, vaise bhi jaane kon de rha!";
 
     catImg.src = "cat_dance.gif";
 
